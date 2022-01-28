92c9ca09196252f8765d6691a41f1ff0e6fb3707
Atanas Naydenov <anaydenov93@gmail.com>
2022-01-28 16:36:37 +0100

    display all posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,10 @@
 class PostsController < ApplicationController
 
   def show
-    
+    @post = Post.find(params[:id])
+  end
+
+  def index
+    @posts = Post.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "feeds#index"  
   resources :feeds
+  resources :posts, only: [:show, :index]
 end
